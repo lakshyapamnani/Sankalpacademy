968695ec045b390ab45b9bf363a1e8bf7b359dd7
lakshya <lakshaypamnani2@gmail.com>
2026-04-09 14:16:59 +0530

    pwa

--- public/sw.js
+++ public/sw.js
@@ -6,8 +6,7 @@ const FILES_TO_CACHE = [
   OFFLINE_URL,
   '/index.html',
   '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  '/icons/Screenshot 2026-04-09 135955.ico'
 ];
 
 self.addEventListener('install', event => {
